629afa24b48cf753da596dd0741d212e3a70ad50
Mark <markporcarojr@gmail.com>
2023-11-27 01:21:11 -0500

    removed bootstrap

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: 'http://localhost:3000/auth/google/callback',
+    callbackURL: 'https://hive-tool.onrender.com/login',
 },
     async (accessToken, refreshToken, profile, done) => {
         const googleId = profile.id;
