6314f3496d094e9dd55cb88707e12ed2be42245b
Mark <markporcarojr@gmail.com>
2024-04-02 21:04:35 -0400

    Try Google Auth

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ const LocalStrategy = require('passport-local').Strategy;
 const User = require('./models/user');
 const flash = require('connect-flash');
 const MongoStore = require('connect-mongo');
-// const Settings = require('./models/settings')
 require('dotenv').config();
 
 // ========================= Middleware =========================
