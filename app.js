6cc00d058552445cf4eb2b6c537de64f2fda2fbf
Mark <markporcarojr@gmail.com>
2024-04-12 22:04:39 -0400

    Fix comma in app.js

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     callbackURL: 'https://apiary-tool.com/auth/google/callback',
-    ,
+
 
 },
     async (accessToken, refreshToken, profile, done) => {
